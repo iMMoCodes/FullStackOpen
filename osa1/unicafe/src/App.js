49ce69892396a83cd3afddc21aa995595f19fac3
iMMoCodes <matti-makela@hotmail.com>
2022-01-06 19:41:00 +0200

    Tehtävä 1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = (good * 1 + neutral * 0 + bad * -1) / total || 0;
-  const positive = (good / total) * 100;
-
+  const positive = (good / total) * 100 || 0;
   return (
-    <div>
-      <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+    <>
       <h1>Statistics</h1>
       <h3>Good {good}</h3>
       <h3>Neutral {neutral}</h3>
@@ -21,6 +13,22 @@ const App = () => {
       <h3>All {total}</h3>
       <h3>Average {average}</h3>
       <h3>Positive {positive} %</h3>
+    </>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
