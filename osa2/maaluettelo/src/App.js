9940dbf04898714a631153cfbd176f966e32bf93
iMMoCodes <matti-makela@hotmail.com>
2022-01-12 21:45:29 +0200

    Tehtävä 2.14

--- osa2/maaluettelo/src/App.js
+++ osa2/maaluettelo/src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const App = () => {
   const [search, setSearch] = useState('');
   const [data, setData] = useState([]);
-  console.log(data);
+  const [weather, setWeather] = useState({});
 
   useEffect(() => {
     if (search !== '') {
@@ -16,6 +16,18 @@ const App = () => {
         .catch((err) => setData([]));
     }
   }, [search]);
+
+  useEffect(() => {
+    if (data.length === 1) {
+      axios
+        .get(
+          `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${search}`
+        )
+        .then((response) => {
+          setWeather(response.data.current);
+        });
+    }
+  }, [data.length, search]);
 
   return (
     <>
@@ -44,15 +56,30 @@ const App = () => {
                 </ul>
                 <img
                   style={{ maxHeight: 300, maxWidth: 300 }}
+                  alt='flag'
                   src={country.flags.png}
                 />
+                {weather && data.length === 1 && (
+                  <>
+                    <h2>Weather in {country.capital}</h2>
+                    <h3>Temperature: {weather.temperature} Celsius</h3>
+                    <img src={weather.weather_icons} alt='weather' />
+                    <h3>
+                      Wind: {weather.wind_speed} mph direction{' '}
+                      {weather.wind_dir}
+                    </h3>
+                  </>
+                )}
               </div>
             );
           })
         ) : (
           data.map((country) => {
             return (
-              <div style={{ display: 'flex', alignItems: 'center' }}>
+              <div
+                key={country.ccn3}
+                style={{ display: 'flex', alignItems: 'center' }}
+              >
                 <h2 key={country.ccn3}>{country.name.common}</h2> &nbsp;
                 <button
                   style={{ height: 30 }}
