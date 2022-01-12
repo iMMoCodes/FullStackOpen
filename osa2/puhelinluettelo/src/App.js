186e84bae94e09d66625c7371c47d13dc095f732
iMMoCodes <matti-makela@hotmail.com>
2022-01-12 04:07:24 +0200

    Tehtävä 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: 1234567 },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addPerson = (e) => {
     e.preventDefault();
     const addedPerson = {
       name: newName,
+      number: newNumber,
     };
     if (persons.some((e) => e.name === newName)) {
       alert(`${newName} is already added to phonebook`);
@@ -15,10 +19,15 @@ const App = () => {
       setPersons(persons.concat(addedPerson));
     }
     setNewName('');
+    setNewNumber('');
   };
 
   const handleNewPerson = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNewNumber = (e) => {
+    setNewNumber(e.target.value);
   };
 
   return (
@@ -29,12 +38,19 @@ const App = () => {
           name: <input value={newName} onChange={handleNewPerson} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h1>Numbers</h1>
       {persons.map((person) => {
-        return <h4 key={person.name}>{person.name}</h4>;
+        return (
+          <h4 key={person.name}>
+            {person.name} {person.number}
+          </h4>
+        );
       })}
     </div>
   );
